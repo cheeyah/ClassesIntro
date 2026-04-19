7f8ac6173e012a9571cb100b56287cad55c36883
Konrad Gadzina <konrad@enklawa.edu.pl>
2026-04-19 13:20:12 +0200

    Handle multiple player instances to show difference between class and struct

--- ClassesIntro/Program.cs
+++ ClassesIntro/Program.cs
@@ -5,30 +5,45 @@
 Vector2 startingPosition = new Vector2(4, 2);
 Player hero = new Player(startingPosition);
 
+List<Player> heroClones = new List<Player>();
+heroClones.Add(hero);
+heroClones.Add(new Player(startingPosition));
+
+foreach (Player clone in heroClones)
+{
+    clone.Display();
+}
+
 while (true)
 {
-    hero.Display();
-    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-    hero.ClearAtPosition();
+    for (int i = 0; i < heroClones.Count; i++)
+    {
+        Player currentClone = heroClones[i];
+        currentClone.Display();
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+        currentClone.ClearAtPosition();
     
-    if (keyInfo.Key == ConsoleKey.A)
-    {
-        // ruch w lewo
-        hero.Move(-1, 0);
-    }
-    else if (keyInfo.Key == ConsoleKey.D)
-    {
-        // ruch w prawo
-        hero.Move(1, 0);
-    }
-    else if (keyInfo.Key == ConsoleKey.W)
-    {
-        // ruch w górę
-        hero.Move(0, -1);
-    }
-    else if (keyInfo.Key == ConsoleKey.S)
-    {
-        // ruch w dół
-        hero.Move(0, 1);
+        if (keyInfo.Key == ConsoleKey.A)
+        {
+            // ruch w lewo
+            currentClone.Move(-1, 0);
+        }
+        else if (keyInfo.Key == ConsoleKey.D)
+        {
+            // ruch w prawo
+            currentClone.Move(1, 0);
+        }
+        else if (keyInfo.Key == ConsoleKey.W)
+        {
+            // ruch w górę
+            currentClone.Move(0, -1);
+        }
+        else if (keyInfo.Key == ConsoleKey.S)
+        {
+            // ruch w dół
+            currentClone.Move(0, 1);
+        }
+        
+        currentClone.Display();
     }
 }
